2ccbf2884fca735f319f4c3e7458457858ef704a
ldang264 <820704815@qq.com>
2021-08-23 21:49:29 +0800

    add dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ COPY dist /usr/share/app/src/dist/
 RUN chmod -R 755 /usr/share/app/src/dist
 COPY nginx/nginx.conf /etc/nginx/conf.d/default.conf
 
-EXPOSE 10000
+EXPOSE 8090
 
 CMD ["nginx","-g","daemon off;"]
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
-const port = process.env.port || process.env.npm_config_port || 10000 // dev port
+'use strict'
+
+const path = require('path'),
+  name = 'vue-cron-generator', // page title
+  port = process.env.port || process.env.npm_config_port || 8090 // dev port
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
   publicPath: './',
+  outputDir: 'dist',
   lintOnSave: true,
   productionSourceMap: false,
   devServer: {
@@ -12,7 +22,7 @@ module.exports = {
     },
     proxy: {
       '/api': {
-        target: `http://127.0.0.1:5210`,
+        target: `http://106.14.246.234:5210`,
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/api'
@@ -20,5 +30,15 @@ module.exports = {
       }
     },
     disableHostCheck: true
+  },
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: name,
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
   }
 }
